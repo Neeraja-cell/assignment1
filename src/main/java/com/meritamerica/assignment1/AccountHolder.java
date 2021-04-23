61a527ceaf57484352e2561540ceee221c7818fb
neeraja <neeraja_paladugu@outlook.com>
2021-04-23 19:05:21 -0400

    added some code

--- src/main/java/com/meritamerica/assignment1/AccountHolder.java
+++ src/main/java/com/meritamerica/assignment1/AccountHolder.java
@@ -1,5 +1,55 @@
 package com.meritamerica.assignment1;
 
 public class AccountHolder {
+	private String firstName;
+	private String middleName;
+	private String lastName;
+	private String ssn;
+	private CheckingAccount checkingAccount;
+	private SavingsAccount savingsAccount;
+	public AccountHolder(String firstName, String middleName, String lastName, String ssn, double checkingAccountOpeningBalance, double savingsAccountOpeningBalance) {
+		this.firstName = firstName;
+		this.middleName = middleName;
+		this.lastName = lastName;
+		ssn = ssn;
+		
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getMiddleName() {
+		return middleName;
+	}
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getSsn() {
+		return ssn;
+	}
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+	public CheckingAccount getCheckingAccount() {
+		return checkingAccount;
+	}
+	public void setCheckingAccount(CheckingAccount checkingAccount) {
+		this.checkingAccount = checkingAccount;
+	}
+	public SavingsAccount getSavingsAccount() {
+		return savingsAccount;
+	}
+	public void setSavingsAccount(SavingsAccount savingsAccount) {
+		this.savingsAccount = savingsAccount;
+	}
+	
 	
 }
--- src/main/java/com/meritamerica/assignment1/MeritAmericaBankApp.java
+++ src/main/java/com/meritamerica/assignment1/MeritAmericaBankApp.java
@@ -2,4 +2,10 @@ package com.meritamerica.assignment1;
 
 public class MeritAmericaBankApp {
 	
+	public static void main(String[]args) {
+		AccountHolder neeraja = new  AccountHolder("Neeraja",  "_", "Paladugu", "12345", 1000, 0);
+		
+		
+	}
+	
 }
